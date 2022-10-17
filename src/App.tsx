6e655605b5e5506c0330c01671666a7864453be5
vineeth sagar <vineeth.sagar@logichive.in>
2022-10-17 21:12:28 +0530

    initializing repository

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import styled from "styled-components"
+const StyledApp =  styled.div`
+  color:blue
+`
+
+function App() {
+  
+  return (
+    <StyledApp>
+        Welcome to component library
+    </StyledApp>
+  )
+}
+
+export default App
